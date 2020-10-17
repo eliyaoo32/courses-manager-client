24407c9e257513184050e8b367d583376012624c
eliyaoo32 <eliyaoo32@gmail.com>
2020-10-17 11:49:52 +0300

    Create web template

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { CssBaseline, ThemeProvider } from '@material-ui/core';
 import App from './containers/App';
 
+import theme from './theme';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
+
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
