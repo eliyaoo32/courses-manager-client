41fe01de2171f1f93b48c276b7475af642408c68
eliyaoo32 <eliyaoo32@gmail.com>
2020-10-18 09:49:12 +0300

    Cleaned chapter list

---
+++ src/components/ExpandedChapter/components.tsx
@@ -0,0 +1,46 @@
+import { withStyles } from "@material-ui/core";
+import MuiAccordion from "@material-ui/core/Accordion";
+import MuiAccordionSummary from "@material-ui/core/AccordionSummary";
+import MuiAccordionDetails from "@material-ui/core/AccordionDetails";
+
+export const Accordion = withStyles({
+  root: {
+    border: "1px solid rgba(0, 0, 0, .125)",
+    boxShadow: "none",
+    "&:not(:last-child)": {
+      borderBottom: 0,
+    },
+    "&:before": {
+      display: "none",
+    },
+    "&$expanded": {
+      margin: "auto",
+    },
+  },
+  expanded: {},
+})(MuiAccordion);
+
+export const AccordionSummary = withStyles({
+  root: {
+    backgroundColor: "rgba(0, 0, 0, .10)",
+    borderBottom: "1px solid rgba(0, 0, 0, .125)",
+    marginBottom: -1,
+    minHeight: 56,
+    "&$expanded": {
+      minHeight: 56,
+    },
+  },
+  content: {
+    "&$expanded": {
+      margin: "12px 0",
+    },
+  },
+  expanded: {},
+})(MuiAccordionSummary);
+
+export const AccordionDetails = withStyles((theme) => ({
+  root: {
+    margin: "0",
+    padding: "0",
+  },
+}))(MuiAccordionDetails);
--- src/components/ExpandedChapter/index.tsx
+++ src/components/ExpandedChapter/index.tsx
@@ -1,76 +1,35 @@
-import React from 'react';
-import { withStyles, Typography, List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
-import MuiAccordion from '@material-ui/core/Accordion';
-import MuiAccordionSummary from '@material-ui/core/AccordionSummary';
-import PlayIcon from '@material-ui/icons/PlayCircleOutline';
-import MuiAccordionDetails from '@material-ui/core/AccordionDetails';
-import Chapter from '../../types/Chapter';
-
-const Accordion = withStyles({
-    root: {
-      border: '1px solid rgba(0, 0, 0, .125)',
-      boxShadow: 'none',
-      '&:not(:last-child)': {
-        borderBottom: 0,
-      },
-      '&:before': {
-        display: 'none',
-      },
-      '&$expanded': {
-        margin: 'auto',
-      },
-    },
-    expanded: {},
-  })(MuiAccordion);
-  
-  const AccordionSummary = withStyles({
-    root: {
-      backgroundColor: 'rgba(0, 0, 0, .10)',
-      borderBottom: '1px solid rgba(0, 0, 0, .125)',
-      marginBottom: -1,
-      minHeight: 56,
-      '&$expanded': {
-        minHeight: 56,
-      },
-    },
-    content: {
-      '&$expanded': {
-        margin: '12px 0',
-      },
-    },
-    expanded: {},
-  })(MuiAccordionSummary);
-  
-  const AccordionDetails = withStyles((theme) => ({
-    root: {
-      padding: theme.spacing(2),
-    },
-  }))(MuiAccordionDetails);
-  
+import React from "react";
+import { Typography, List, ListItem, ListItemIcon, ListItemText } from "@material-ui/core";
+import PlayIcon from "@material-ui/icons/PlayCircleOutline";
+import useStyles from './styles';
+import { Accordion, AccordionDetails, AccordionSummary } from './components';
+import Chapter from "../../types/Chapter";
 
 interface Props {
-    expanded: boolean,
-    chapter: Chapter
+  expanded: boolean;
+  chapter: Chapter;
 }
 
-function ExpandedChapter({expanded, chapter}: Props) {
-    return (
-        <Accordion square expanded={expanded}>
-            <AccordionSummary>
-                <Typography>{chapter.name}</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-                <List>
-                    <ListItem button>
-                        <ListItemIcon>
-                            <PlayIcon />
-                        </ListItemIcon>
-                        <ListItemText primary="How to start" />
-                    </ListItem>
-                </List>
-            </AccordionDetails>
-        </Accordion>
-    );
+function ExpandedChapter({ expanded, chapter }: Props) {
+  const classes = useStyles();
+
+  return (
+    <Accordion square expanded={expanded}>
+      <AccordionSummary>
+        <Typography>{chapter.name}</Typography>
+      </AccordionSummary>
+      <AccordionDetails>
+        <List className={classes.episodeLists}>
+          <ListItem button>
+            <ListItemIcon>
+              <PlayIcon />
+            </ListItemIcon>
+            <ListItemText primary="How to start" />
+          </ListItem>
+        </List>
+      </AccordionDetails>
+    </Accordion>
+  );
 }
 
 export default ExpandedChapter;
