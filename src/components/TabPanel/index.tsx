ac7fee379b4450374a7d05b5ab6441e8d356a561
eliyaoo32 <eliyaoo32@gmail.com>
2020-10-24 12:27:37 +0300

    Created tab panel component

---
+++ src/components/TabPanel/index.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Typography, Box } from '@material-ui/core';
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: any;
+  value: any;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`wrapped-tabpanel-${index}`}
+      aria-labelledby={`wrapped-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+export default TabPanel;
